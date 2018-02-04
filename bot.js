c122330cb2a57f68bc9ad1f44dfc656597fb64cf
Vinh Tran <truongvinht@gmail.com>
2018-02-04 23:27:50 +0100

    update quick command

--- bot.js
+++ bot.js
@@ -4,6 +4,9 @@ const mobaCounter = require("./mobaCounter.json");
 
 const bot = new Discord.Client({disableEveryone: true});
 
+//counter picker
+const cp = require('./vgCounterPicker');
+
 bot.on("ready", async () => {
   console.log(`Bot is ready! ${bot.user.username}`);
   
@@ -13,23 +16,22 @@ bot.on("ready", async () => {
   }catch(e) {
     console.log(e.stack);
   }
-  
 });
 
 bot.on("message", async message => {
-  
+
   if(message.author.bot) return;
-  
+
   //check for direct message
   if(message.channel.type === "dm") return;
-  
+
   if(!message.content.startsWith(botSettings.prefix)) return;
-  
+
   let messageArray = message.content.split(" ");
-  
+
   let command = messageArray[0];
   let args = messageArray.slice(1);
-  
+
   if (command === `${botSettings.prefix}help`) {
     let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
@@ -38,149 +40,120 @@ bot.on("message", async message => {
     .addField(`${botSettings.prefix}c HERO-CODE`,"Quick Display weakness of given hero code")
     .addField(`${botSettings.prefix}support HERO`,"Displays the strength of hero")
     .addField(`${botSettings.prefix}s HERO-CODE`,"Quick Display strength of given hero code")
+    .addField(`${botSettings.prefix}g HERO-CODE`,"Quick Display strength/weakness of given hero code")
     .addField(`${botSettings.prefix}hero`,"Display list of available heroes");
     message.channel.send(embed);
   }
-  
+
   if (messageArray.length >= 2) {
-  
+    
+    let hero = messageArray[1].toLowerCase();
+    
     // counter pick
     if (command === `${botSettings.prefix}counter`) {
-      message.channel.send(counter(messageArray[1].toLowerCase(),message.author.username));
+      
+      var d = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("#ff0000");
+      
+      let result = cp.getCounter(hero);
+      
+      if (result != null) {
+        message.channel.send(d.addField(`${hero} is weak against`,result));
+      } else {
+        message.channel.send(d.setDescription(`Hero '${hero}' not found!`))
+      }
     }
-    
+
     // quick counter pick
-    if (command === `${botSettings.prefix}c`) {
+    if (command.toLowerCase() === `${botSettings.prefix}c` ) {
+      
+      var d = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("#ff0000");
       
       //hero quick name
       let hName = messageArray[1].toLowerCase();
       
-      if (hName.length == 2) {
-        if (mobaCounter.hero.hasOwnProperty(hName)) {
+      let heroName = cp.getHeroName(hName);
+      
+      if (heroName != null) {
         
-          let hero = mobaCounter.hero[hName];
-        
-          message.channel.send(counter(hero.toLowerCase(),message.author.username));
+        let result = cp.getCounter(heroName.toLowerCase());
+      
+        if (result != null) {
+          message.channel.send(d.addField(`${heroName} is weak against`,result));
         } else {
-          message.channel.send(new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setDescription(`'${hName}' not found!`));
+          message.channel.send(d.setDescription(`Hero '${heroName}' not found!`))
         }
       } else {
           message.channel.send(new Discord.RichEmbed()
             .setAuthor(message.author.username)
-            .setDescription(`'${hName}': Invalid hero code!`));
+            .setDescription(`'${heroName}': Invalid hero code!`));
       }
     }
-  
+
     // support pick
     if (command === `${botSettings.prefix}support`) {
-      message.channel.send(support(messageArray[1].toLowerCase(),message.author.username));
+      
+      var d = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("#008000");
+
+      let result = cp.getSupport(hero);
+      
+      if (result != null) {
+        message.channel.send(d.addField(`${hero} is strong against`,result));
+      } else {
+        message.channel.send(d.setDescription(`Hero '${hero}' not found!`))
+      }
     }
-    
-    
+
+
     // quick counter pick
-    if (command === `${botSettings.prefix}s`) {
+    if (command.toLowerCase() === `${botSettings.prefix}s` ) {
+      
+      var d = new Discord.RichEmbed()
+      .setAuthor(message.author.username)
+      .setColor("#008000");
       
       //hero quick name
       let hName = messageArray[1].toLowerCase();
+      let heroName = cp.getHeroName(hName);
       
-      if (hName.length == 2) {
-        if (mobaCounter.hero.hasOwnProperty(hName)) {
-        
-          let hero = mobaCounter.hero[hName];
-        
-          message.channel.send(support(hero.toLowerCase(),message.author.username));
+      if (heroName != null) {
+        let result = cp.getSupport(heroName.toLowerCase());
+      
+        if (result != null) {
+          message.channel.send(d.addField(`${heroName} is weak against`,result));
         } else {
-          message.channel.send(new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setDescription(`'${hName}' not found!`));
+          message.channel.send(d.setDescription(`Hero '${heroName}' not found!`))
         }
       } else {
           message.channel.send(new Discord.RichEmbed()
             .setAuthor(message.author.username)
-            .setDescription(`'${hName}': Invalid hero code!`));
+            .setDescription(`'${heroName}': Invalid hero code!`));
       }
     }
-    
+
   } else {
     // show heroes list
     if (command === `${botSettings.prefix}hero`) {
-      message.channel.send(heroList(message.author.username));
+      
+      var d = new Discord.RichEmbed()
+      .setAuthor(message.author.username);
+      
+      let keyValueMap = cp.getHeroes();
+      message.channel.send(d.addField(keyValueMap.title,keyValueMap.content));
     }
   }
 });
 
-//bot.login(botSettings.token);
-// Heroku token
-bot.login(process.env.BOT_TOKEN);
 
-// weak against
-function counter(hero, username) {
-  
-  var d = new Discord.RichEmbed()
-  .setAuthor(username)
-  .setColor("#ff0000");
-    
-  //check for hero name
-  if (mobaCounter.against.hasOwnProperty(hero)) {
-  
-    if (mobaCounter.against[hero].length > 0) {
-      var list = "";
-      
-      for(var name of mobaCounter.against[hero]) {
-        list = list + (`- ${name}`) + "\n";
-      }
-      
-      d = d.addField(`${hero} is weak against`,list);
-      return d;
-    }
-  } 
-  // hero not found
-  return new Discord.RichEmbed()
-    .setAuthor(username)
-    .setDescription(`Hero '${hero}' not found!`);
-}
-
-// strong against
-function support(hero, username) {
-  
-  var d = new Discord.RichEmbed()
-  .setAuthor(username)
-  .setColor("#008000");
-    
-  //check for hero name
-  if (mobaCounter.with.hasOwnProperty(hero)) {
-  
-    if (mobaCounter.with[hero].length > 0) {
-      var list = "";
-      
-      for(var name of mobaCounter.with[hero]) {
-        list = list + (`+ ${name}`) + "\n";
-      }
-      
-      return d.addField(`${hero} is strong against`,list);
-    }
-  } 
-  // hero not found
-  return new Discord.RichEmbed()
-    .setAuthor(username)
-    .setDescription(`Hero '${hero}' not found!`);
-}
-
-// hero list
-function heroList(username) {
-  
-  var d = new Discord.RichEmbed()
-  .setAuthor(username);
-  
-  // list for output
-  var list = "";
-  
-  
-  for (var key of Object.keys(mobaCounter.hero)) {
-    list = list+ "+ " + mobaCounter.hero[key] + " [" + `${key}` + "]\n";
-  }
-  
-  return d.addField(`List of available heroes [${Object.keys(mobaCounter.hero).length}]`,list);
+if (botSettings.token != "") {
+  // use local TOKEN from settings
+  bot.login(botSettings.token);
+} else {
+  // Heroku ENV token
+  bot.login(process.env.BOT_TOKEN);
 }
