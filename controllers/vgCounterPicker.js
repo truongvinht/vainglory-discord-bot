0168191bfa805d202e5af61926aaf84a7fefc6db
Vinh Tran <truongvinht@gmail.com>
2018-03-06 15:15:59 +0100

    readjust heroes file

--- controllers/vgCounterPicker.js
+++ controllers/vgCounterPicker.js
@@ -6,79 +6,93 @@
 // import 
 const vgCounter = require("../data/heroes.json");
 const i18n = require('./langSupport');
+var log = require('loglevel');
 
 // weak against
 var counter = function(hero) {
-    
-  //check for hero name
-  if (vgCounter.against.hasOwnProperty(hero)) {
-  
-    if (vgCounter.against[hero].length > 0) {
-      var list = "";
-      
-      for(var name of vgCounter.against[hero]) {
-        list = list + (`- ${name}`) + "\n";
-      }
-      return list;
+
+    //check for hero name
+    if (vgCounter.against.hasOwnProperty(hero)) {
+
+        if (vgCounter.against[hero].length > 0) {
+            var list = "";
+            for (var name of vgCounter.against[hero]) {
+                const fullName = quickHeroLookup(name);
+                if (fullName!=null) {
+                    list = list + (`- ${fullName}`) + "\n";
+                } else {
+                    log.error(`- ${name}`)
+                }
+            }
+            return list;
+        }
     }
-  } 
-  
-  // hero not found
-  return null;
+
+    // hero not found
+    return null;
 }
 
 // strong against
 var support = function(hero) {
-  
-  //check for hero name
-  if (vgCounter.with.hasOwnProperty(hero)) {
-  
-    if (vgCounter.with[hero].length > 0) {
-      var list = "";
-      
-      for(var name of vgCounter.with[hero]) {
-        list = list + (`+ ${name}`) + "\n";
-      }
-      
-      return list;
+
+    //check for hero name
+    if (vgCounter.with.hasOwnProperty(hero)) {
+
+        if (vgCounter.with[hero].length > 0) {
+            var list = "";
+
+            for (var name of vgCounter.with[hero]) {
+                const fullName = quickHeroLookup(name);
+                if (fullName!=null) {
+                    list = list + (`+ ${fullName}`) + "\n";
+                } else {
+                    log.error(`+ ${name}`)
+                }
+            }
+
+            return list;
+        }
     }
-  } 
-  // hero not found
-  return null;
+    // hero not found
+    return null;
 }
 
 // hero list
 var heroes = function() {
-  
-  // list for output
-  var list = "";
-  
-  for (var key of Object.keys(vgCounter.hero)) {
-    list = list+ "+ " + vgCounter.hero[key] + " [" + `${key}` + "]\n";
-  }
-  
-  let content = {
-    "title": `${i18n.get('ListAvailableHeroes')} [${Object.keys(vgCounter.hero).length}]`,
-    "content": list
-  }
-  
-  return content;
+
+    // list for output
+    var list = "";
+
+    for (var key of Object.keys(vgCounter.hero)) {
+        list = list + "+ " + vgCounter.hero[key] + " [" + `${key}` + "]\n";
+        
+        //Test for all heroes
+        // support(quickHeroLookup(key));
+        // counter(quickHeroLookup(key));
+    }
+
+    let content = {
+        "title": `${i18n.get('ListAvailableHeroes')} [${Object.keys(vgCounter.hero).length}]`,
+        "content": list
+    }
+
+    return content;
 }
 
 var quickHeroLookup = function(hero) {
-  if (hero.length == 2) {
-    if (vgCounter.hero.hasOwnProperty(hero)) {
-      return vgCounter.hero[hero];
+    if (hero.length == 2) {
+        if (vgCounter.hero.hasOwnProperty(hero)) {
+            return vgCounter.hero[hero];
+        }
+    } else {
+        return null;
     }
-  }else {
-    return null;
-  }
 }
 
 // export
 module.exports = {
-  getCounter: counter,
-  getSupport: support, 
-  getHeroes: heroes,
-  getHeroName: quickHeroLookup
+    getCounter: counter,
+    getSupport: support,
+    getHeroes: heroes,
+    getHeroName: quickHeroLookup
 };
